5264ca3c61ab5bd9908f98a4eabcbe294c14e720
George Santos <george_diu@hotmail.com>
2023-04-18 19:45:52 -0300

    Subindo o projeto novo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+
+{{--@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">You are logged in!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @stop --}}
+
+{{--@extends('adminlte::page') --}}
+@extends('adminlte::page', ['iFrameEnabled' => true])
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
